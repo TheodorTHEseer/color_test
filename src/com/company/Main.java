23d6ecdc269649ca6e68daa6ec9097cbbe64933c
TheodorTHEseer <fedor.prozorovV@gmail.com>
2022-10-29 23:42:29 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+            System.out.printf("\u001b[31mR \u001b[32mG \u001b[34mB \u001b[0mreset \u001b[37mwhite \u001b[30mblack");
+            System.out.printf("\n\u001b[0mR G B reset white black");
+
+            if(args.length>0){
+                for (int i=0; i < args.length; i++)
+                    System.out.printf(args[i]+"\n");
+            }
+        }
+}
