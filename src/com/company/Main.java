150f68646beb632e6ef0131e9269594281232931
TheodorTHEseer <fedor.prozorovV@gmail.com>
2022-10-30 22:37:42 +0300

    -switch/case added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,43 @@ package com.company;
 
 import com.company.functoins.Paint;
 
+import java.util.Arrays;
+
 public class Main {
 
         public static void main(String[] args) {
+
+            for (String str:args
+                 ) {
+                switch (str.toLowerCase()){
+                    case "--paint":paint(args);
+                    case "--server":server(args);
+                    case "--text":server(args);
+                }
+            }
 
             for (int i=0; i<args.length; i+=2){
                 Paint paint = new Paint(args[i], args[i+1]);
                 paint.getResult();
             }
+        }
+
+        private static void paint(String[] strings){
+            for (int i=0; i<strings.length; i+=2){
+                String tag = strings[i];
+                String option = strings[i+1];
+                if (tag.equalsIgnoreCase("--server")||tag.equalsIgnoreCase("--text"))
+                    break;
+                Paint paint = new Paint(tag, option);
+                paint.getResult();
+            }
+        }
+
+        private static void server (String [] strings){//plug
+            System.out.println(strings.toString()+" -server !started");
+        }
+
+        private static void text(String [] strings){//plug
+            System.out.println(strings.toString()+" -was text");
         }
 }
