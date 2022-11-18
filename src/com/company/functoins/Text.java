b6eefd10afbf1caea51d3b26a758bcf9f37d6eaa
TheodorTHEseer <fedor.prozorovV@gmail.com>
2022-11-18 11:27:52 +0300

    Refactoring: -added Color class

--- src/com/company/functoins/Text.java
+++ src/com/company/functoins/Text.java
@@ -1,4 +1,12 @@
 package com.company.functoins;
 
+import com.company.utils.Color;
+
 public class Text {
+
+    private String text;
+    private String label;
+    private Color color;
+
+
 }
---
+++ src/com/company/utils/Color.java
@@ -0,0 +1,10 @@
+package com.company.utils;
+
+import java.util.HashMap;
+
+public class Color {
+    private String name;
+    private final static HashMap <String, String> nameColorMap = new HashMap<>(20);
+
+
+}
